171619558758d5ff50ac190be39049a0e7105c02
Scott Fleming <fleming@stsci.edu>
2017-11-27 13:25:48 -0500

    - K2VARCAT now gets data from /hlsps/ (new area)

--- parse_obsid_hlsp_k2varcat.py
+++ parse_obsid_hlsp_k2varcat.py
@@ -56,7 +56,7 @@ def parse_obsid_hlsp_k2varcat(obsid):
 
     # Use the observation ID to get paths to each file.
     dir_root = (os.path.pardir + os.path.sep + os.path.pardir + os.path.sep +
-                "missions" + os.path.sep + "hlsp" + os.path.sep + "k2varcat" +
+                "hlsps" + os.path.sep + "k2varcat" +
                 os.path.sep + campaign + os.path.sep)
     star_dir_root = (k2varcatid[0:4] + "00000" + os.path.sep + k2varcatid[4:6] +
                      "000" + os.path.sep)
